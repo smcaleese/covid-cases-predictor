623b46e38847ac9c5b616970a50e52d8492234f6
Stephen McAleese <stephenmcaleese11@gmail.com>
2020-07-02 17:43:18 +0100

    Updates

---
+++ precache-manifest.6440be7e5ad967552eac402a9ca32c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9b28b3edc08bade1912243cecf693c",
+    "url": "/covid-cases-predictor/index.html"
+  },
+  {
+    "revision": "122f000f114ca695c579",
+    "url": "/covid-cases-predictor/static/css/main.07be2ba8.chunk.css"
+  },
+  {
+    "revision": "a2f1baa730c3ce647acf",
+    "url": "/covid-cases-predictor/static/js/2.8b6b26e2.chunk.js"
+  },
+  {
+    "revision": "e12ed09be89d98e3b07b38f179289140",
+    "url": "/covid-cases-predictor/static/js/2.8b6b26e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122f000f114ca695c579",
+    "url": "/covid-cases-predictor/static/js/main.02db4bd3.chunk.js"
+  },
+  {
+    "revision": "21ef7f870cb7925414d0",
+    "url": "/covid-cases-predictor/static/js/runtime-main.dc984e4a.js"
+  }
+]);
